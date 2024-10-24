9a7fff3ea0d12dc8704b2a2b42a484ae00a03de4
Otávio Sbampato <otaviosbampato@gmail.com>
2024-10-24 20:41:13 -0300

    formatação A.java

--- A.java
+++ A.java
@@ -1,20 +1,20 @@
-public class A{
+public class A {
     private int A1;
     private double A2;
 
-    public int getA1(){
+    public int getA1() {
         return A1;
     }
 
-    public double getA2(){
+    public double getA2() {
         return A2;
     }
 
-    public void MA1(){
+    public void MA1() {
         System.out.println("MA1");
     }
 
-    public void MA2(){
+    public void MA2() {
         System.out.println("MA2");
     }
 
